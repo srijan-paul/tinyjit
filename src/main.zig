848f3ec8b4b19a9ab7bd816111455a3b824825ff
injuly <srijannr1@gmail.com>
2024-08-05 20:43:06 +0530

    jumping and labels

--- src/main.zig
+++ src/main.zig
@@ -5,23 +5,42 @@ const Opcode = enum(u8) {
     add,
     print,
     exit,
+    eq, // pushes 1 if stack[-1] == stack[-2], 0 otherwise
+    jumpif_1,
+    jump,
+};
+
+const CodeBlock = struct {
+    instructions: []const u8,
+    constants: []const i64,
 };
 
 const Interpreter = struct {
     stack: [32_000]i64 = undefined,
     stack_pos: u64 = 0,
 
-    instructions: []const u8,
+    blocks: []const CodeBlock,
+    current_block: *const CodeBlock = undefined,
+
     pc: usize = 0,
 
-    constants: []const i64,
+    pub fn init(blocks: []const CodeBlock) Interpreter {
+        return .{
+            .blocks = blocks,
+            .current_block = &blocks[0],
+        };
+    }
 
     inline fn loadConst(self: *Interpreter) i64 {
-        const index = self.instructions[self.pc];
-        const constant = self.constants[index];
+        const index = self.current_block.instructions[self.pc];
+        const constant = self.current_block.constants[index];
 
         self.pc += 1;
         return constant;
+    }
+
+    inline fn top(self: *Interpreter) i64 {
+        return self.stack[self.stack_pos - 1];
     }
 
     inline fn pop(self: *Interpreter) i64 {
@@ -34,10 +53,15 @@ const Interpreter = struct {
         self.stack_pos += 1;
     }
 
+    inline fn nextOp(self: *Interpreter) u8 {
+        const op = self.current_block.instructions[self.pc];
+        self.pc += 1;
+        return op;
+    }
+
     pub fn run(self: *Interpreter) !void {
-        while (self.pc < self.instructions.len) {
-            const instr: Opcode = @enumFromInt(self.instructions[self.pc]);
-            self.pc += 1;
+        while (self.pc < self.current_block.instructions.len) {
+            const instr: Opcode = @enumFromInt(self.nextOp());
 
             switch (instr) {
                 .load_const => self.push(self.loadConst()),
@@ -51,8 +75,29 @@ const Interpreter = struct {
                     const exit_code: u8 = @intCast(self.pop());
                     std.process.exit(exit_code);
                 },
+                .eq => {
+                    const a = self.pop();
+                    const b = self.pop();
+                    self.push(if (a == b) 1 else 0);
+                },
+
+                .jump => self.jump(),
+                .jumpif_1 => {
+                    if (self.pop() == 1) {
+                        self.jump();
+                    } else {
+                        self.pc += 1;
+                    }
+                },
             }
         }
+    }
+
+    fn jump(self: *Interpreter) void {
+        const block_idx = self.nextOp();
+
+        self.current_block = &self.blocks[block_idx];
+        self.pc = 0;
     }
 };
 
@@ -61,17 +106,30 @@ pub inline fn Op(o: Opcode) u8 {
 }
 
 pub fn main() !void {
-    var interpreter = Interpreter{
+    const b0 = CodeBlock{
         .instructions = &[_]u8{
-            Op(.load_const), 0, // load 42
-            Op(.load_const), 1, // load 24
-            Op(.add), // 42 + 24 = 66
-            Op(.print), // print(66)
-            Op(.load_const), 2, // load 0
-            Op(.exit), // exit()
+            Op(.load_const), 0, // load 5
+            Op(.load_const), 1, // load 5
+            Op(.add), // 11
+            Op(.load_const), 2, // load 10
+            Op(.eq),
+            Op(.jumpif_1), 1, // jump to b1 if eq
+            Op(.load_const), 3, // load 1
+            Op(.exit),
         },
-        .constants = &[_]i64{ 42, 24, 0 },
+        .constants = &[_]i64{ 5, 5, 10, 1 },
     };
 
+    const b1 = CodeBlock{
+        .instructions = &[_]u8{
+            Op(.load_const), 0, // load 0
+            Op(.exit),
+        },
+        .constants = &[_]i64{0},
+    };
+
+    const program = [_]CodeBlock{ b0, b1 };
+
+    var interpreter = Interpreter.init(&program);
     try interpreter.run();
 }
