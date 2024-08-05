96ccec0c36306b795126837e1598be7b1938cee7
injuly <srijannr1@gmail.com>
2024-08-05 21:19:15 +0530

    looping

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,15 @@
 const std = @import("std");
 
 const Opcode = enum(u8) {
-    load_const,
+    push,
     add,
     print,
     exit,
     eq, // pushes 1 if stack[-1] == stack[-2], 0 otherwise
     jumpif_1,
     jump,
+    load_var,
+    store_var,
 };
 
 const CodeBlock = struct {
@@ -64,7 +66,7 @@ const Interpreter = struct {
             const instr: Opcode = @enumFromInt(self.nextOp());
 
             switch (instr) {
-                .load_const => self.push(self.loadConst()),
+                .push => self.push(self.loadConst()),
                 .add => {
                     const a = self.pop();
                     const b = self.pop();
@@ -89,6 +91,17 @@ const Interpreter = struct {
                         self.pc += 1;
                     }
                 },
+
+                .load_var => {
+                    const index = self.nextOp();
+                    self.push(self.stack[index]);
+                },
+
+                .store_var => {
+                    // [value, index]
+                    const index = self.nextOp();
+                    self.stack[index] = self.pop();
+                },
             }
         }
     }
@@ -106,29 +119,52 @@ pub inline fn Op(o: Opcode) u8 {
 }
 
 pub fn main() !void {
+    // 0 is x, 1 is i
     const b0 = CodeBlock{
         .instructions = &[_]u8{
-            Op(.load_const), 0, // load 5
-            Op(.load_const), 1, // load 5
-            Op(.add), // 11
-            Op(.load_const), 2, // load 10
-            Op(.eq),
-            Op(.jumpif_1), 1, // jump to b1 if eq
-            Op(.load_const), 3, // load 1
-            Op(.exit),
+            Op(.push), 0, // x = 0
+            Op(.push), 0, // i = 0
+            Op(.jump), 1, // jump to while loop (block 1
         },
-        .constants = &[_]i64{ 5, 5, 10, 1 },
+
+        .constants = &[_]i64{0},
     };
 
     const b1 = CodeBlock{
         .instructions = &[_]u8{
-            Op(.load_const), 0, // load 0
+            // while i < 1_000_000
+            Op(.load_var), 1, // load i
+            Op(.push), 0, // load 1_000_000
+            Op(.eq),
+            Op(.jumpif_1), 2, // jump to exit if i == 1_000_000
+
+            // x = x + 1
+            Op(.load_var), 0, // load x
+            Op(.load_var), 1, // load i
+            Op(.add),
+            Op(.store_var), 0, // x = x + i
+
+            // i += 1
+            Op(.load_var), 1, // load i
+            Op(.push), 1, // load 1
+            Op(.add),
+            Op(.store_var), 1, // i = i + 1
+            Op(.jump), 1, // jump to start
+        },
+        .constants = &[_]i64{ 1_00_0001, 1 },
+    };
+
+    const b2 = CodeBlock{
+        .instructions = &[_]u8{
+            Op(.load_var), 0, // load x
+            Op(.print),
+            Op(.push), 0, // load 0
             Op(.exit),
         },
         .constants = &[_]i64{0},
     };
 
-    const program = [_]CodeBlock{ b0, b1 };
+    const program = [_]CodeBlock{ b0, b1, b2 };
 
     var interpreter = Interpreter.init(&program);
     try interpreter.run();
